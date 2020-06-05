c0f95ef885b59adbe8b9e0863a4fb5e8d1b83535
Emelyn Freire <emelyn_pg@hotmail.com>
2020-06-05 03:11:12 -0300

    Códigos JS do formulário e menu

---
+++ js/formulario_contato.js
@@ -0,0 +1,10 @@
+
+function Enviar() {
+ 
+    var nome = document.getElementById("nomeid");
+ 
+    if (nome.value != "") {
+        alert('Dados enviados com sucesso!! Responderemos em breve, desde já gratos pelo contato.');
+    }
+ 
+}
