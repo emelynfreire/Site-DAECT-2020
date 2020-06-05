b5405cb376f18897a89d909303e44d429b7bd368
Emelyn Freire <emelyn_pg@hotmail.com>
2020-06-05 17:47:16 -0300

    código php pra enviar pelo método POST

---
+++ php/enviarEmail.php
@@ -0,0 +1,28 @@
+<?php
+//1 – Definimos Para quem vai ser enviado o email
+$para = "organizareagirdaect@gmail.com";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST['nome'];
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel 
+//$assunto
+$email = $_POST['email'];
+$assunto = $_POST['assunto'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Nome:  </strong>".$nome;
+$mensagem .= "<br>  <strong>Mensagem: </strong>"
+.$_POST['mensagem'];
+ 
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  daect.ect.ufrn.br<daect@daect.ect.ufrn.br>\n"; 
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <daect@daect.ect.ufrn.br>\n"; 
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <daect@daect.ect.ufrn.br>\n"; 
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+ 
+mail($para,$nome,$email, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+?>
